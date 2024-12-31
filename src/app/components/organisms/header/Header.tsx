898c260a1dba9dc30a6ed9f9c936b35526e87ea8
balckowl <shiggyjr1492@gmail.com>
2024-12-31 12:26:29 +0900

    feat: headerを作成

---
+++ src/app/components/organisms/header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+	return (
+		<header className="flex h-[80px] items-center border-b">
+			<h1 className="font-bold text-[24px]">Prompt Builder</h1>
+		</header>
+	);
+}
